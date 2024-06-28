4c564371da41ab8cb99c1c1ec23fefedef621182
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-06-28 18:28:18 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:chat_light_dark_fe_fl/screens/welcome/welcome_screen.dart';
+import 'package:chat_light_dark_fe_fl/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat layout light and dark theme',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      themeMode: ThemeMode.light,
+      home: const WelcomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/chats/components/body.dart
@@ -0,0 +1,49 @@
+import 'package:chat_light_dark_fe_fl/components/filled_outline_button.dart';
+import 'package:chat_light_dark_fe_fl/constants.dart';
+import 'package:chat_light_dark_fe_fl/models/Chat.dart';
+import 'package:chat_light_dark_fe_fl/screens/messages/message_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(
+              kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
+          color: kPrimaryColor,
+          child: Row(
+            children: [
+              FillOutlineButton(press: () {}, text: "Recent Message"),
+              const SizedBox(width: kDefaultPadding),
+              FillOutlineButton(
+                press: () {},
+                text: "Active",
+                isFilled: false,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: chatsData.length,
+            itemBuilder: (context, index) => ChatCard(
+              chat: chatsData[index],
+              press: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const MessagesScreen(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
